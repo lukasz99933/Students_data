1fc964f74bfeda3b6d162edd779cbbebd4d33889
lukasz99933 <lukasz8126700@gmail.com>
2018-02-02 21:35:09 +0100

    Poprawa, brak wycieków.

--- student.cpp
+++ student.cpp
@@ -7,10 +7,18 @@ using namespace std;
 
 struct Student
 {
+	Student(string a,string b, int c) :
+		forename(a),
+		surname(b),
+		number_index(c)
+	{}
+
 	string forename;
 	string surname;
 	int number_index;
 };
+
+
 bool compareBynumberindex(const Student &a,const Student &b)
 {
 	return a.number_index < b.number_index;
@@ -32,18 +40,9 @@ void remove(int a,vector<Student> &b)
 int main()
 {
 	vector<Student> data;
-	Student* person1 = new Student; 
-	person1->forename = "Mirosław";
-	person1->surname = "Łęcina";
-	person1->number_index = 226531;
-	Student* person2 = new Student;
-       	person2->forename = "Dominik";
-	person2->surname = "Jahaś";
-	person2->number_index = 226321;
-	Student* person3 = new Student;
-	person3->forename = "Kapitan";
-	person3->surname = "Bomba";
-	person3->number_index = 226555;
+	Student* person1 = new Student("Mirosław","Łęcina",226531); 
+	Student* person2 = new Student("Dominik","Jahaś",226321);
+	Student* person3 = new Student("Kapitan","Bomba",226555);
 	data.push_back(*person1);
 	data.push_back(*person2);
 	data.push_back(*person3);
@@ -52,6 +51,9 @@ int main()
 	cout << data[1].forename << endl;
 	cout << data[2].forename << endl;
 	remove(226555,data);
+	delete person1;
+	delete person2;
+	delete person3;
 	return 0;
 
 }
